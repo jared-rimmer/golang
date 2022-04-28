64f9286cc9c4b9adca47089ec29a75f2d8c97a6b
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-04-28 07:28:20 +0100

    First commit!

---
+++ interfaces.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func measure(g shape) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+}
+
+func main() {
+	r := rectangle{width: 3, height: 4}
+	c := circle{radius: 5}
+
+	measure(r)
+	measure(c)
+}
